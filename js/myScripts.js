6e3e4cf8b8f567179f48eb51b3321e1c25cb5e8f
zvanclee <zachvancleef@gmail.com>
2020-12-13 15:15:25 -0500

    Update myScripts.js

--- js/myScripts.js
+++ js/myScripts.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         document.getElementById("myBtn").style.display = "block";
     } else {
         document.getElementById("myBtn").style.display = "none";
