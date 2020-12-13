e5f398cbeab07dd23171b3ba12e166864250802f
zvanclee <zachvancleef@gmail.com>
2020-12-13 15:30:21 -0500

    Update myScripts.js

--- js/myScripts.js
+++ js/myScripts.js
@@ -9,6 +9,7 @@ function scrollFunction() {
     }
 }
 
+
 // When the user clicks on the button, scroll to the top of the document
 function backToTopFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
