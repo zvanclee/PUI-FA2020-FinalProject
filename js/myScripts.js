b6369e88e21f6e5e286f3ea098bba11919841a1c
zvanclee <zachvancleef@gmail.com>
2020-12-13 14:51:57 -0500

    desc

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,19 +1,18 @@
-//Get the button
-var backToTopButton = document.getElementById("backToTop-button");
+//Get the button:
+mybutton = document.getElementById("backToTop-button");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTopButton.style.display = "block";
+    mybutton.style.display = "block";
   } else {
-    backToTopButton.style.display = "none";
+    mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function backToTopFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
