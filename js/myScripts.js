5e03381a4cec5fc10c372fed5e1f4755b24cb041
zvanclee <zachvancleef@gmail.com>
2020-12-13 15:27:38 -0500

    desc

--- js/myScripts.js
+++ js/myScripts.js
@@ -10,7 +10,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function backToTopFunction() {
     document.body.scrollTop = 0; // For Chrome, Safari and Opera
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
